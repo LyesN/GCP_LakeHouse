9b09606c9179be875e5c1b1595412cf08637dcf7
Lyes NADOUR <lyes.nadour@gmail.com>
2025-09-09 13:29:32 +0200

    Add files via upload

---
+++ load_csv_employees.sql
@@ -0,0 +1,16 @@
+-- Flux d'ingestion CSV vers BigQuery
+-- Fichier source : employees
+-- Table cible : employees_5mb.csv
+
+LOAD DATA INTO `lake-471013.lakehouse_employee_data.employees`
+(id INT64, nom STRING, prenom STRING, email STRING, age INT64, ville STRING, 
+ code_postal STRING, telephone STRING, salaire FLOAT64, departement STRING, 
+ date_embauche DATE, statut STRING, score FLOAT64, latitude FLOAT64, 
+ longitude FLOAT64, commentaire STRING, reference STRING, niveau STRING, 
+ categorie STRING, timestamp TIMESTAMP)
+FROM FILES (
+  format = 'CSV',
+  field_delimiter = ';',
+  skip_leading_rows = 1,
+  uris = ['gs://lakehouse-bucket-20250903/employees_5mb.csv']
+);
